d87ed2e8a6a274bfe7c92d342bcb145c0de2589b
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-19 22:15:17 +0530

    Update and rename B_Multiple_Buttons.ino to B_Buttons_Colour_Brightness.ino

---
+++ B_Buttons_Colour_Brightness.ino
@@ -0,0 +1,19 @@
+/*Two button i */
+
+
+
+
+int redPin = 9, greenPin = 10, bluePin = 11;
+int brightnessPin = 3, colourPin = 5;
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(brightnessPin, INPUT);
+  pinMode(brightness, INPUT);
+}
+void loop()
+{
+  
+}
--- B_Multiple_Buttons.ino
+++
@@ -1,1 +0,0 @@
-/*Two Button control. Each button is a digital input - HIGH when pressed */
