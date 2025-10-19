bd77dac191cb054de5ce29ae8d483ab5a0f08614
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-19 18:54:53 +0530

    Update B_Button_Control_LED.ino

--- B_Button_Control_LED.ino
+++ B_Button_Control_LED.ino
@@ -1,6 +1,6 @@
-/*Button is a digital input: HIGH(pressed) or LOW(not pressed). Arduino reads thr button state using digitalRead() and controls the LED with digitalWrite() accordingly.
+/*Button is a digital input: HIGH(pressed) or LOW(not pressed). Arduino reads the button state using digitalRead() and controls the LED with digitalWrite() accordingly.
 CIRCUIT - LED anode(+) to pin 9 through 220 ohm resistor, LED cathode to GND. Connect one button pin to pin 2, other button pin to 5V. 
-Add 10 kilo ohm resistor between pin 2 and GND (pull-down resistor ensures thr pin reads LOW when button is not pressed)*/
+Add 10 kilo ohm resistor between pin 2 and GND (pull-down resistor ensures the pin reads LOW when button is not pressed)*/
 
 int ledPin = 9, buttonPin = 2, buttonState = 0;
 void setup()
