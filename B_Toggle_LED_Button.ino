97cb9497f5f94769a25cbe11eb7da27ed1ac46f8
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-19 19:04:56 +0530

    Update B_Toggle_LED_Button.ino

--- B_Toggle_LED_Button.ino
+++ B_Toggle_LED_Button.ino
@@ -1,1 +1,3 @@
-//Each press toggles LED state. Introduces variables and logic memory
+/*Instead of the LED bring ON only while its pressed, a toggle button makes it switch states with each press. Every time the button is pressed, the LED changes its state ON -> OFF -> ON
+We need to detect the transition (button going from not pressed -> pressed), not just whether it's being held.
+*/
