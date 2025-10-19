286c90e2b104cf4d766d88646200a1f3d5742276
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-19 13:21:32 +0530

    Update A_LED_Pattern.ino

--- A_LED_Pattern.ino
+++ A_LED_Pattern.ino
@@ -4,7 +4,25 @@ CIRCUIT - Anodes(+) of 10 LEDs to Arduino pins through a 220 ohm resistor, and a
 
 int ledPins[] = {1,2,3,4,5,6,7,8,9,10}; //Array. LED pins
 int numLED = 10;
+
 void setup()
 {
   for(int i=1; i<=10; i++)
     pinMode(ledPins[i], OUTPUT)
+}
+
+void loop()
+{
+  for(int i=1; i<=numLED; i++) //Forward sequence
+    {
+      digitalWrite(ledPins[i], HIGH);
+      delay(200);
+      digitalWrite(ledPins[i], LOW);
+    }
+  for(int i=numLED-1; i>=2; i--) //Reverse sequence. This loop controls only LEDs 9-2 so that LED 1&10 don't blink twice during direction change
+    {
+      digitalWrite(ledPins[i], HIGH);
+      delay(200);
+      digitalWrite(ledPins[i], LOW);
+    }
+}
