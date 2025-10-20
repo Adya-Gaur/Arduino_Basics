823560a58113e0e7ed9e3c91fa3735c98277bd0a
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-20 16:55:45 +0530

    Update C_IR_Sensor_Test.ino

--- C_IR_Sensor_Test.ino
+++ C_IR_Sensor_Test.ino
@@ -1,1 +1,5 @@
-//Detect object or black/white surface. Print values on Serial Monitor
+/*An IR sensor works using infrared light reflections. It has two eyes - IR LED (transmitter) and one photodiode (receiver). The transmitter sends out invusuble IR light. If there's
+an object or white surface, IR light reflects back -> sensor detects it. If the surface is black or away -> IR light gets absorbed. So:
+Object/White - Output LOW(0) ; No Object/Black -> Output HIGH(1)
+
+CIRCUIT - */
