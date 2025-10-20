0183b5da011fa42eca1625da84bf5b9668f5fab9
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-20 11:20:39 +0530

    Update B_Buttons_Colour_Brightness.ino

--- B_Buttons_Colour_Brightness.ino
+++ B_Buttons_Colour_Brightness.ino
@@ -89,4 +89,4 @@ void loop()
 }
 
 /*Honest Thoughts - Handling two independent inputs, two logical modes (colour and brightness), function stucturing and PWM-based output control - in short,
-designing a system that responds dynamically - it felt a little tough. But, the moment it worked, it felt magical &bso rewarding! */
+designing a system that responds dynamically - it felt a little tough. But, the moment it worked, it felt magical and so rewarding! */
