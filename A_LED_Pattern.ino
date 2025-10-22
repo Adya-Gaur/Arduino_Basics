e9d0aecfbd73252ba107e20108cdf41d17323ed4
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-22 19:42:51 +0530

    Update A_LED_Pattern.ino

--- A_LED_Pattern.ino
+++ A_LED_Pattern.ino
@@ -2,7 +2,7 @@
 Each LED will ligh up for a short time, then the next one, and so on.
 CIRCUIT - Anodes(+) of 10 LEDs to Arduino pins through a 220 ohm resistor, and all cathodes(-) go to GND*/
 
-int ledPins[] = {1,2,3,4,5,6,7,8,9,10}; //Array. LED pins
+int ledPins[] = {2,3,4,5,6,7,8,9,10,11}; //Array. LED pins
 int numLED = 10;
 
 void setup()
@@ -13,13 +13,13 @@ void setup()
 
 void loop()
 {
-  for(int i=1; i<=numLED; i++) //Forward sequence
+  for(int i=2; i<=11; i++) //Forward sequence
     {
       digitalWrite(ledPins[i], HIGH);
       delay(200);
       digitalWrite(ledPins[i], LOW);
     }
-  for(int i=numLED-1; i>=2; i--) //Reverse sequence. This loop controls only LEDs 9-2 so that LED 1&10 don't blink twice during direction change
+  for(int i=9; i>=3; i--) //Reverse sequence. This loop controls only LEDs connected to pin 10-3 so that terminal LED connected to pin 2&11 don't blink twice during direction change
     {
       digitalWrite(ledPins[i], HIGH);
       delay(200);
