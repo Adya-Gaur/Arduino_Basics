b4809f021b52429b1b38cc6266b6cea28689c826
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-02 22:35:06 +0530

    Update C_LDR_LightSensor.ino

--- C_LDR_LightSensor.ino
+++ C_LDR_LightSensor.ino
@@ -3,23 +3,35 @@ resistance increases -> voltage increases.
 If it's dark -> LED turns ON & if it's dark bright -> turn LED OFF 
 CIRCUIT - One leg of LDR -> 5V, other leg of LDR -> A0 and 10 kilo ohm resistor. The other end of the 10 kilo ohm -> GND. LED(+) -> pin 9 via 220 ohm resistor. LED(-) -> GND*/
 
-int ldrPin = A0, ledPin = 9, ldrValue = 0;
-int threshold = 500; //Adjust based on lighting
+#include <LiquidCrystal.h>
 
-void setup()
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int ldrPin = A0, ldrValue = 0;
+
+void setup() 
 {
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("LDR Levels");
+  delay(1500);
+  lcd.clear();
 }
 
-void loop()
+void loop() 
 {
-  ldrValur = analogRead(ldrPin);
-  Serial.println(ldrValue); //For calibration
-  
-  if(ldrValue < threshold) //it's dark
-    digitalWrite(ledPin, HIGH);
-  else //it's bright
-    digitalWrite(ledPin, LOW);
-  delay(200);
+  ldrValue = analogRead(ldrPin);
+  lcd.setCursor(0, 0);
+  lcd.print("Light: ");
+  lcd.print(ldrValue);
+  lcd.print("   ");   // clears extra digits
+
+  lcd.setCursor(0, 1);
+  if (ldrValue < 300) 
+    lcd.print("Status: DARK   ");
+  else if (ldrValue < 700) 
+    lcd.print("Status: MEDIUM ");
+  else 
+    lcd.print("Status: BRIGHT ");
+
+  delay(1000);
 }
