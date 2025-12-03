b9ff6e693b969e3709c031c9c52b1070d3b35a21
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-03 21:24:10 +0530

    Update C_FlameSensor.ino

--- C_FlameSensor.ino
+++ C_FlameSensor.ino
@@ -3,23 +3,22 @@
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
 int flamePin = A0;   // analog input
 
-void setup() {
+void setup() 
+{
   lcd.begin(16, 2);
   lcd.print("Flame Sensor");
 }
 
-void loop() {
+void loop() 
+{
   int value = analogRead(flamePin);
-
   lcd.setCursor(0, 1);
-  if (value > 700) {            // threshold for flame
+  if (value > 700)  // threshold for flame
     lcd.print("Flame Detected ");
-  } else {
+  else 
     lcd.print("No Flame       ");
-  }
 
   delay(200);
 }
