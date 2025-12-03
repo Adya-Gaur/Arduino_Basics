9970ca53368f6ba1b80d784b4a88ef4677ff7b3d
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-03 11:43:32 +0530

    Update C_IR_Sensor_Test.ino

--- C_IR_Sensor_Test.ino
+++ C_IR_Sensor_Test.ino
@@ -2,4 +2,34 @@
 an object or white surface, IR light reflects back -> sensor detects it. If the surface is black or away -> IR light gets absorbed. So:
 Object/White - Output LOW(0) ; No Object/Black -> Output HIGH(1)
 
-CIRCUIT - */
+CIRCUIT - VCC - 5V, OUT - Pin 8, GND - GND Pin */
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int irPin = 8, irValue;
+
+void setup() 
+{
+  lcd.begin(16, 2);
+  pinMode(irPin, INPUT);
+  lcd.print("IR Sensor Test");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() 
+{
+  irValue = digitalRead(irPin);
+  lcd.setCursor(0, 0);
+  lcd.print("IR Value: ");
+  lcd.print(irValue);
+  lcd.print("  "); // clear extra
+  lcd.setCursor(0, 1);
+  
+  if (irValue == LOW) 
+    lcd.print("Obstacle: YES  ");  // MOST IR modules give LOW when obstacle detected
+  else 
+    lcd.print("Obstacle: NO   ");
+  
+  delay(1000);
+}
