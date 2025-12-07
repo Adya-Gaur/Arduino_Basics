b6284e373d5c6c4b4a49274dc87ce3962032cb50
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-07 17:12:12 +0530

    Update circuit description in D_DC_Button+Potentiometer.ino

--- D_DC_Button+Potentiometer.ino.ino
+++ D_DC_Button+Potentiometer.ino.ino
@@ -1,2 +1,2 @@
 /*Button toggles the direction of mootor spin and potentiometer knob is used to control the speed of the motor.
-  CIRCUIT - */
+  CIRCUIT - Motor wires -> OUT1 and OUT2 of driver, L298N - ENA -> Pin 5, IN1 -> Pin 7, IN2 - Pin 8, 12V and GND -> battery case, */
