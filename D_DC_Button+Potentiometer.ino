06fd888f4d414c24f78d819d390a5ba286160ba3
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-07 17:24:02 +0530

    Update comments for button and potentiometer connections

--- D_DC_Button+Potentiometer.ino
+++ D_DC_Button+Potentiometer.ino
@@ -1,3 +1,3 @@
 /*Button toggles the direction of mootor spin and potentiometer knob is used to control the speed of the motor.
   CIRCUIT - Motor wires -> OUT1 and OUT2 of driver, L298N - ENA -> Pin 5, IN1 -> Pin 7, IN2 - Pin 8, 12V and GND -> battery case, Potentiometer - Left pin → 5V, 
-  Right pin → GND, Middle pin → A0, */
+  Right pin → GND, Middle pin → A0,  button pin -> pin 2, other button pin -> 5V, 10 kilo ohm resistor between pin 2 and GND*/
