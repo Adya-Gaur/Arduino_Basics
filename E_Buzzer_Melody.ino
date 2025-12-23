a20f23556dd5d94f1a797c74b1fbb94b2d51518d
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-23 17:53:23 +0530

    Implement success and error tone functions
    Added functions for success and error tones using the buzzer.

--- E_Buzzer_Melody.ino
+++ E_Buzzer_Melody.ino
@@ -1,1 +1,44 @@
+/*Using Passive buzzer to generate different tunes.
+  CIRCUIT - Buzzer anode -> Pin 9, Buzzer cathode -> GND*/
 
+int buzzer = 9;
+void setup() 
+{
+  tone(buzzer, 523);   // C5
+  delay(200);
+  noTone(buzzer);
+  delay(50);
+  tone(buzzer, 659);   // E5
+  delay(200);
+  noTone(buzzer);
+  delay(50);
+  tone(buzzer, 784);   // G5
+  delay(300);
+  noTone(buzzer);
+
+  void errorTone();
+  void successTone();
+}
+
+void successTone() 
+{
+  tone(9, 1000);
+  delay(150);
+  tone(9, 1500);
+  delay(150);
+  noTone(9);
+}
+void errorTone() 
+{
+  for(int i = 0; i < 3; i++) 
+  {
+    tone(9, 300);
+    delay(150);
+    noTone(9);
+    delay(100);
+  }
+}
+
+void loop() 
+{  
+}
