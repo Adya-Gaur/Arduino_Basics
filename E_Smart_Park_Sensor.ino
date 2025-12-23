ba72eb1263eb4b591406d25bb92523bea360e310
Adya Gaur <adyagaurofficial@gmail.com>
2025-12-23 21:42:13 +0530

    Update E_Smart_Park_Sensor.ino

--- E_Smart_Park_Sensor.ino
+++ E_Smart_Park_Sensor.ino
@@ -1,1 +1,30 @@
 //Ultrasonic + LED/Buzzer. Alert when an object is close
+
+int trigPin = 5, echoPin = 6, buzzer = 2, distance;
+long duration; 
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (0.034 * duration)/2; 
+
+  if(distance <= 5)
+    digitalWrite(buzzer, HIGH);
+  else
+    digitalWrite(buzzer, LOW);
+  
+  delay(50);
+}
